e254eb6d821c4e6b0421f12e727f4be18098dcc8
RLCM696 <robertoldelacruzmondejar@gmail.com>
2024-11-10 02:07:03 +0000

    Sat, Nov 9, 2024, 9:07 PM -05:00

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 from flask import redirect, render_template, session
 from functools import wraps
@@ -21,3 +22,11 @@ def login_required(f):
 def pending(value: list[dict]):
     # Return the number of pending tasks in a list of tasks
     return sum(["Finished" not in val["status"] for val in value])
+
+def date_tool(value: str):
+    # Convert string to datetime object
+    date_obj = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+
+    # Convert datetime object to different format
+    new_date_str = date_obj.strftime("%d/%m/%Y %I:%M %p")
+    return
